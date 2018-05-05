d17dd97f147d2c1bf38628f554a60c565b249eee
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-05-05 13:58:28 +0200

    cleaning up

--- src/main/scala/example/Score.scala
+++ src/main/scala/example/Score.scala
@@ -4,9 +4,9 @@ import scalaz._
 object Score {
   sealed trait ScoreTag
   type Score = Int @@ ScoreTag
-  def apply(score: Int): Score = Tag[Int, ScoreTag](score)
   implicit def scoreToValue(score: Score): Int = Tag.unwrap(score)
   implicit def scoreFromValue(score: Int): Score = Score(score)
+  def apply(score: Int): Score = Tag[Int, ScoreTag](score)
   implicit object ScoreMonoid extends Monoid[Score] {
     def zero: Score = Score(0)
     def append(v1: Score, v2: => Score): Score = Score(v1 + v2)
